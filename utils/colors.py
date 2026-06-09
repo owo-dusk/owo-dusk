56e182bcd0d477bac6bc9bd648fb60e55a5ebeca
Chillbytefire <chillbytefire@gmail.com>
2026-06-09 15:15:47 +0530

    create utils/system.py and setup.py refactors

---
+++ utils/colors.py
@@ -0,0 +1,8 @@
+class COLORS:
+    RESET = "\033[m"
+    BOLD_GREEN = "\033[1;32m"
+    BOLD_RED = "\033[1;31m"
+    BOLD_YELLOW = "\033[1;33m"
+    BOLD_CYAN = "\033[1;36m"
+    BOLD_BLUE = "\033[1;34m"
+    BOLD_MAGENTA = "\033[1;35m"
