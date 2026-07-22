9bc054972739792d76eddb3b2699687b5b01f7a3
EchoQuill <92152439+EchoQuill@users.noreply.github.com>
2026-07-22 10:32:55 +0530

    Fix: battery.py invalid console import
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- utils/system/battery.py
+++ utils/system/battery.py
@@ -16,8 +16,8 @@ import threading
 import time
 
 from utils.errors import suppress_and_log, suppress_and_log_block
-from utils.loader import console, global_settings_dict, misc_dict
-from utils.system.system import console_width, on_mobile
+from utils.loader import global_settings_dict, misc_dict
+from utils.system.system import console, console_width, on_mobile
 
 psutil = None
 
