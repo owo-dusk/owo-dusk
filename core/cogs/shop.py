37444047e845c9e8a1311f48390f91725be055b8
EchoQuill <safnaj059@gmail.com>
2026-09-09 21:30:48 +0530

    chore: version + format

--- core/cogs/shop.py
+++ core/cogs/shop.py
@@ -77,9 +77,7 @@ class Shop(BaseCog):
             self.cmd["cmd_arguments"] = item
             await self.bot.ch.put_queue(self.cmd)
         else:
-            await self.bot.sleep_till(
-                self.bot.settings_dict.cooldowns.briefCooldown
-            )
+            await self.bot.sleep_till(self.bot.settings_dict.cooldowns.briefCooldown)
             await self.send_buy()
 
     @commands.Cog.listener()
